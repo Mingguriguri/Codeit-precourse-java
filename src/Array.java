73182260a1eafc2c50ee57d60832b2154128d03e
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-05-02 18:34:50 +0900

    [JAVA] 기본 내용 폴더 파일 구조 재구조화

---
+++ src/Array.java
@@ -0,0 +1,24 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i*10;
+        }
+        for (int k: intArray) {
+            System.out.println(k);
+        }
+
+        String[] fruitsArray = new String[5];
+
+        fruitsArray[0] = "딸기";
+        fruitsArray[1] = "당근";
+        fruitsArray[2] = "수박";
+        fruitsArray[3] = "참외";
+        fruitsArray[4] = "메론";
+
+        for (String fruit : fruitsArray) {
+            System.out.println(fruit);
+        }
+
+    }
+}
