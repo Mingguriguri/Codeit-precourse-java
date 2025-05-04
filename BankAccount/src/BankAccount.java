69ec5b3c00e775e2ef6378e3055c4c6d6367dddd
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-05-04 18:37:59 +0900

    BankAccount 기반 실습 - 객체 만들기(클래스, 메소드, 인스턴스), 입금출금 메소드, 접근제어자, 메소드 오버라이딩, 계좌이체, 생성자

---
+++ BankAccount/src/BankAccount.java
@@ -0,0 +1,107 @@
+public class BankAccount {
+    private int balance;
+    private Person owner;
+
+    public BankAccount(int pBalance) {
+        if(pBalance < 0) {
+            pBalance = 0;
+        }
+        balance = pBalance;
+    }
+
+    public BankAccount(Person pOwner) {
+        balance = 0;
+        owner = pOwner;
+    }
+
+    public BankAccount(int pBalance, Person pOwner) {
+        if(pBalance < 0) {
+            pBalance = 0;
+        }
+        balance = pBalance;
+        owner = pOwner;
+    }
+
+
+    public void setBalance(int newBalance) {
+        if (newBalance >= 0) {
+            balance = newBalance;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setOwner(Person newPerson) {
+        owner = newPerson;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    // 입금 메서드
+    // 파라미터: 입금할 액수 (정수)
+    // 리턴: 성공여부 (불린)
+    boolean deposit(int amount) {
+        int cashAmount = owner.getCashAmount();
+        if (amount < 0 || cashAmount < amount) {
+            System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + cashAmount + "원");
+            return false;
+        }
+
+        balance += amount;
+        owner.setCashAmount(cashAmount - amount);
+        System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+        return true;
+
+    }
+
+    // 출금 메서드
+    // 파라미터: 출금할 액수 (정수)
+    // 리턴: 성공여부 (불린)
+    boolean withdraw(int amount) {
+        int cashAmount = owner.getCashAmount();
+        if (amount < 0 || balance < amount) {
+            System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + cashAmount + "원");
+            return false;
+        }
+
+        balance -= amount;
+        owner.setCashAmount(cashAmount - amount);
+        System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+        return true;
+    }
+
+    // 첫 번째 파라미터: 받는 사람 (Person)
+    // 두 번째 파라미터: 이체할 금액 (정수)
+    // 리턴 : 성공여부 (불린)
+    public boolean transfer(Person to, int amount) {
+        return transfer(to.getAccount(), amount);
+    }
+
+    // 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
+    // 두 번째 파라미터: 이체할 금액 (정수)
+    // 리턴 : 성공여부 (불린)
+    public boolean transfer(BankAccount to, int amount) {
+        boolean success;
+        if (amount < 0 || amount > balance) {
+            success = false;
+        }
+        else {
+            // "나"의 계좌에서 빠져나가기
+            balance -= amount;
+            // 상대 계좌에 이채하기
+            to.balance += amount;
+            success = true;
+        }
+
+        System.out.println(success + " - from: " + owner.getName()
+                + ", to: " + to.getOwner().getName()
+                + ", amount: " + amount
+                + ", balance: " + balance
+        );
+        return success;
+    }
+}
---
+++ BankAccount/src/Person.java
@@ -0,0 +1,77 @@
+public class Person {
+    private String name;
+    private int age;
+    private int cashAmount;
+    private BankAccount account;
+
+    public Person(String pName, int pAge) {
+        if (pAge < 0) {
+            pAge = 12;
+        }
+        name = pName;
+        age = pAge;
+        cashAmount = 0; // 정수형의 기본 값은 0이므로, 이 부분은 없어도 됨
+    }
+
+    public Person(String pName, int pAge, int pCashAmount) {
+        if (pAge < 0) {
+            pAge = 12;
+        }
+        if (pCashAmount < 0) {
+            pCashAmount = 0;
+        }
+        name = pName;
+        age = pAge;
+        cashAmount = pCashAmount;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0) {
+            age = newAge;
+        }
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setCashAmount(int newCashAmount) {
+        if (newCashAmount >= 0) {
+            this.cashAmount = newCashAmount;
+        }
+    }
+
+    public int getCashAmount() {
+        return cashAmount;
+    }
+
+    public void setAccount(BankAccount newAccount){
+        this.account = newAccount;
+    }
+
+    public BankAccount getAccount(){
+        return account;
+    }
+
+    // 첫 번째 파라미터: 받는 사람 (Person)
+    // 두 번째 파라미터: 이체할 금액 (정수)
+    // 리턴 : 성공여부 (불린)
+    public boolean transfer(Person to, int amount) {
+        return account.transfer(to, amount);
+    }
+
+    // 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
+    // 두 번째 파라미터: 이체할 금액 (정수)
+    // 리턴 : 성공여부 (불린)
+    public boolean transfer(BankAccount to, int amount) {
+        return account.transfer(to, amount);
+    }
+
+}
